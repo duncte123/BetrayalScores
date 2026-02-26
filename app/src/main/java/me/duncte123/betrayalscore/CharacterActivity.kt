83c35e1ea44e5e2fa51c7164332d28076aebafbe
duncte123 <contact@duncte123.me>
2026-02-26 20:29:00 +0100

    AAAAAAAAA

--- app/src/main/java/me/duncte123/betrayalscore/CharacterActivity.kt
+++ app/src/main/java/me/duncte123/betrayalscore/CharacterActivity.kt
@@ -12,13 +12,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -113,7 +110,6 @@ fun CharacterPolygonBackground(character: Character, modifier: Modifier = Modifi
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatisticSlider(stat: Statistic) {
     var sliderPosition by remember { mutableIntStateOf(stat.defaultScoreIndex) }
